1b64991a8e76e36007538df24bd5693473b13563
Romulusdagger <romulusdagger3798@gmail.com>
2023-10-04 13:59:35 +0800

    update the server url

--- client/script.js
+++ client/script.js
@@ -96,7 +96,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv)
 
-  const response = await fetch('https://sun-nqbt.onrender.com', {
+  const response = await fetch('https://codex-nu7z.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
